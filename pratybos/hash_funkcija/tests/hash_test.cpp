1dc9e2038911a5d4b37696910e57a5b4597af021
Kristupas Arifovas <50778281+Quikmove@users.noreply.github.com>
2025-09-21 09:55:59 +0300

    feat: add mock tests (#1)

---
+++ pratybos/hash_funkcija/tests/hash_test.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(HashTest, BasicAssertions) {
+    EXPECT_EQ(1 + 1, 2);
+}
---
+++ pratybos/hash_funkcija/tests/hasher_test.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(HasherTest, BasicAssertions) {
+    EXPECT_EQ(1, 1);
+    EXPECT_TRUE(true);
+}
