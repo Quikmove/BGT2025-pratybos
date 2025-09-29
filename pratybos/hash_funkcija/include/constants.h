a5440355e701332f8abcddb2022cba771b9e0957
Kristupas Arifovas <50778281+Quikmove@users.noreply.github.com>
2025-09-29 21:11:33 +0300

    refactor: change constants to inline variables in constants.h, use conventional k prefix

--- pratybos/hash_funkcija/include/constants.h
+++ pratybos/hash_funkcija/include/constants.h
@@ -2,8 +2,8 @@
 #include <filesystem>
 
 
-const std::filesystem::path test_dir = "test_files";
-const std::filesystem::path empty_file = test_dir / "empty.txt";
-const std::filesystem::path symbol_path = test_dir / "symbol";
-const std::filesystem::path collision_path = test_dir / "collision";
-const std::filesystem::path avalanche_path = test_dir / "avalanche";
+inline const std::filesystem::path test_dir = "test_files";
+inline const std::filesystem::path empty_file = test_dir / "empty.txt";
+inline const std::filesystem::path symbol_path = test_dir / "symbol";
+inline const std::filesystem::path collision_path = test_dir / "collision";
+inline const std::filesystem::path avalanche_path = test_dir / "avalanche";
