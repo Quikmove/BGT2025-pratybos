95410dcc04c518f946d3ba54209acc5174f8ad17
Kristupas Arifovas <50778281+Quikmove@users.noreply.github.com>
2025-09-29 21:38:50 +0300

    fix: add guard rails for write to file function

--- pratybos/hash_funkcija/include/FileWrite.h
+++ pratybos/hash_funkcija/include/FileWrite.h
@@ -2,6 +2,13 @@
 #include <fstream>
 inline void Write(std::filesystem::path filePath, std::string payload) {
     std::ofstream oss(filePath);
+    if(oss.fail()) {
+        throw std::filesystem::filesystem_error(
+            "file not found",
+            filePath,
+            std::make_error_code(std::errc::no_such_file_or_directory)
+        );
+    }
     oss << payload;
     oss.close();
 }
