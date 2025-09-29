b9da5050d5f8a2a0b36c2cde5f7dfc4c8ac50900
Kristupas Arifovas <50778281+Quikmove@users.noreply.github.com>
2025-09-29 21:17:56 +0300

    refactor: update variable names in constants.h to use conventional k prefix

--- pratybos/hash_funkcija/include/constants.h
+++ pratybos/hash_funkcija/include/constants.h
@@ -2,8 +2,8 @@
 #include <filesystem>
 
 
-inline const std::filesystem::path test_dir = "test_files";
-inline const std::filesystem::path empty_file = test_dir / "empty.txt";
-inline const std::filesystem::path symbol_path = test_dir / "symbol";
-inline const std::filesystem::path collision_path = test_dir / "collision";
-inline const std::filesystem::path avalanche_path = test_dir / "avalanche";
+inline const std::filesystem::path kTestDir = "test_files";
+inline const std::filesystem::path kEmptyFile = kTestDir / "empty.txt";
+inline const std::filesystem::path kSymbolPath = kTestDir / "symbol";
+inline const std::filesystem::path kCollisionPath = kTestDir / "collision";
+inline const std::filesystem::path kAvalanchePath = kTestDir / "avalanche";
