d867b0e103514702990c5e913a8c76e0320db1ec
Kristupas Arifovas <50778281+Quikmove@users.noreply.github.com>
2025-10-01 19:54:26 +0300

    refactor: clean up hash test by removing commented code and using instance method for hashing

--- pratybos/hash_funkcija/tests/hash_test.cpp
+++ pratybos/hash_funkcija/tests/hash_test.cpp
@@ -1,8 +1,7 @@
 #include <Hasher.h>
 #include <gtest/gtest.h>
-
-// TEST(HashTest, BasicAssertions) { EXPECT_EQ(1 + 1, 2); }
-
+#include <constants.h>
+Hasher hasher;
 
 TEST(HashTest, DeterministicResults) {
   const std::vector<std::string> words = {"asadda",
@@ -11,10 +10,9 @@ TEST(HashTest, DeterministicResults) {
                                           "maasaasda",
                                           "iasnoiasndoiasndionasdionasidn",
                                           "asjiasdiasdjasd"};
-
   for (const auto &word : words) {
-    auto hash1 = Hasher::hash256bit(word);
-    auto hash2 = Hasher::hash256bit(word);
+    auto hash1 = hasher.hash256bit(word);
+    auto hash2 = hasher.hash256bit(word);
     EXPECT_EQ(hash1, hash2);
   }
 }
